b2dc9d3e01082e8c0f61c425b0b6d61d881c7bde
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-10-31 11:18:17 +0300

    add navbar

--- frontend/src/components/layouts/Header.tsx
+++ frontend/src/components/layouts/Header.tsx
@@ -1,7 +1,21 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
+import icon from "@/icon.jpg";
+import { Button } from "../ui/button";
+
 export function Header() {
   return (
-    <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur">
-      gchfhfj
+    <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur p-3">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <img src={icon} className="w-10"></img>
+          <NavigationMenuItem>
+            <Button variant={"default"}>Dashboard</Button>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Button variant={"default"}>Sample</Button>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </header>
   );
 }
