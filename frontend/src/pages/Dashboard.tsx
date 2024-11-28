51dd6d2bf28f3e9228073f287759d307dcbdc8c7
Wayedintr <boranbereketli@gmail.com>
2024-11-28 21:17:20 +0300

    API test

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,21 @@
+import React from "react";
+import { useState } from "react";
+
 export default function Dashboard() {
-  return <>Home</>;
+  const [info, setInfo] = useState<{ message: string } | null>(null);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then(res => res.json())
+      .then(data => setInfo(data));
+    
+  }, []);
+  React.useEffect(() => {
+    console.log(info)
+  }, [info]);
+  return (
+    <>
+      <h1>Home</h1>
+      {info && <p>{info.message}</p>}
+    </>
+  );
 }
