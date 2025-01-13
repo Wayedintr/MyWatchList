1e058cd8ea7d041ed11a288a16011a8524677d5d
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2025-01-14 02:32:51 +0300

    fix footer position

--- frontend/src/components/layouts/AppLayout.tsx
+++ frontend/src/components/layouts/AppLayout.tsx
@@ -4,16 +4,14 @@ import { Footer } from "./Footer";
 
 export function Applayout() {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <div className="flex-grow flex flex-col">
-        <div className="container px-4 md:px-8 flex-grow flex flex-col">
-          <Outlet />
-        </div>
+      <div className="container px-4 md:px-8 flex-grow flex flex-col">
+        <Outlet />
       </div>
       <div className=" pt-10 px-0">
         <Footer />
       </div>
-    </>
+    </div>
   );
 }
