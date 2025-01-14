7307c5a4e4a3f84940be2cce127eabebcb83c2ce
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2025-01-15 02:45:00 +0300

    icon goes to home

--- frontend/src/components/layouts/Header.tsx
+++ frontend/src/components/layouts/Header.tsx
@@ -31,7 +31,9 @@ export function Header() {
 
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur md:px-64 px-8 py-3 flex justify-between">
-      <img src={icon} className="w-10"></img>
+      <Link to="/">
+        <img src={icon} className="w-10" />
+      </Link>
 
       <NavigationMenu>
         <NavigationMenuList>
